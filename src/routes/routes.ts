e9229371890cbb56f73048f9fad18ad261539df0
andreassef <andreassef02@gmail.com>
2021-04-25 19:21:48 -0400

    [ADD] - Nova rota para criacao de user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,10 +2,13 @@ import { response, Router } from "express";
 import { getCustomRepository } from "typeorm";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 import { SettingsController } from "../controllers/SettingsController";
+import { UsersController } from "../controllers/UsersController";
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
 routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
 
 export { routes };
